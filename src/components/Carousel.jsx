f3cf86966ea23a907e9089364ea1242185ed4698
ignacioborraz <ignacioborraz@hotmail.com>
2023-06-05 19:21:00 -0300

    fix

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -5,7 +5,7 @@ export default function Carousel({ title,character_photo,cover_photo,pepe }) {
     const d_right = "M12.75 15l3-3m0 0l-3-3m3 3h-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
     return (
         <div className='flex items-center justify-evenly bg-gradient-to-r from-[#F9A8D4] to-[#F472B6] rounded-lg mt-5 hidden px-5
-        md:flex md:h-56'>
+        md:flex md:h-56 md:w-full'>
             <Arrow d={d_left} />
             <img className='h-64 self-end ' src={character_photo}/>
             <img className='h-56 mb-12 sel-end' src={cover_photo}/>
