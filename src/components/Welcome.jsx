2b01756b1ce3bc0050605fe654475ea568825563
ignacioborraz <ignacioborraz@hotmail.com>
2023-06-14 00:21:46 -0300

    fix

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -8,7 +8,7 @@ export default function Welcome() {
             <p className='text-2xl mb-1 text-center'>Find the perfect manga for you</p>
             <p className='text-2xl hidden font-bold mb-2
             md:flex'>#MingaForever 🔥</p>
-            <Anchor to='/signin' className="border-none bg-gradient-to-r from-[#F9A8D4] to-[#F472B6] w-40 p-2 justify-center text-xl rounded-lg">Sign In!</Anchor>
+            <Anchor to='/signin' className="border-none bg-gradient-to-r from-[#F9A8D4] to-[#F472B6] w-40 p-2 flex justify-center md:justify-start text-xl rounded-lg">Sign In!</Anchor>
         </div>
     )
     
