1ef9e3f5cd800f4682143a7e7544f053a6238baa
IGNACIO JAVIER BORRAZ <95113020+ignacioborraz@users.noreply.github.com>
2023-06-16 00:11:01 -0300

    rrd+refs (#9)

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,7 +1,23 @@
-import { Link as Anchor } from "react-router-dom"
+import { Link as Anchor,useNavigate } from "react-router-dom"
+import { useRef } from "react"
 
 export default function SignIn() {
     
+    const navigate = useNavigate()
+    const signin = ()=> {
+        let data = {
+            email: email.current.value,
+            password: password.current.value
+        }
+        console.log(data)
+        //data se envía en la petición POST al backend
+        //redirigir en caso de éxito
+        //mostrar alertas en caso de fracaso
+        setTimeout(()=>navigate('/'),5000)
+    }
+    const email = useRef()
+    const password =  useRef()
+
     return (
         <main className="flex w-full min-h-screen items-center justify-between">
             <img className="hidden md:block md:absolute md:top-0 md:left-0 h-screen w-[50%] object-cover" src="/img/signin.png" alt="signin" />
@@ -9,9 +25,10 @@ export default function SignIn() {
                 <p className="font-semibold text-[32px] text-center">Welcome <span className="text-[#F472B6]">back</span>!</p>
                 <p className="font-semibold text-[12px] mb-[12px] text-center p-2">Discover manga and manwha, track your progress, have fun, read manga.</p>
                 <form className="flex flex-col my-[25px]">
-                    <input className="w-[260px] md:w-[300px] lg:w-[360px] xl:w-[440px] h-[60px] p-2 my-[12px] text-[12px] rounded-lg border-2 border-[#1F1F1F]" type="email" name="email" id="email" placeholder="Email" />
-                    <input className="w-[260px] md:w-[300px] lg:w-[360px] xl:w-[440px] h-[60px] p-2 my-[12px] text-[12px] rounded-lg border-2 border-[#1F1F1F]" type="password" name="password" id="password" placeholder="Password" />
-                    <input className="w-[260px] md:w-[300px] lg:w-[360px] xl:w-[440px] h-[60px] p-2 my-[12px] text-xl text-white rounded-lg bg-gradient-to-r from-[#F9A8D4] to-[#F472B6]" type="button" value="Sign in" />
+                    {/* con ref={email} estoy asignando una referencia a un input */}
+                    <input ref={email} className="w-[260px] md:w-[300px] lg:w-[360px] xl:w-[440px] h-[60px] p-2 my-[12px] text-[12px] rounded-lg border-2 border-[#1F1F1F]" type="email" name="email" id="email" placeholder="Email" />
+                    <input ref={password} className="w-[260px] md:w-[300px] lg:w-[360px] xl:w-[440px] h-[60px] p-2 my-[12px] text-[12px] rounded-lg border-2 border-[#1F1F1F]" type="password" name="password" id="password" placeholder="Password" />
+                    <input onClick={signin} className="w-[260px] md:w-[300px] lg:w-[360px] xl:w-[440px] h-[60px] p-2 my-[12px] text-xl text-white rounded-lg bg-gradient-to-r from-[#F9A8D4] to-[#F472B6]" type="button" value="Sign in" />
                 </form>
                 <p className="font-semibold text-[12px] mt-[12px] text-center p-2">You don't have an account yet? <Anchor to='/register' className="text-[#F472B6]">Register</Anchor>!</p>
                 <p className="font-semibold text-[12px] text-center p-2">Go back to <Anchor to='/' className="text-[#F472B6] hover:text-black">Home Page</Anchor>!</p>
