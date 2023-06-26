744bca33c464f28f2230d3835dae801294ae2f8c
ignacioborraz <ignacioborraz@hotmail.com>
2023-06-26 20:02:57 -0300

    url

--- src/apiUrl.js
+++ src/apiUrl.js
@@ -2,7 +2,7 @@ let apiUrl = 'http://localhost:8080/api'
 
 console.log(import.meta.env.VITE_VERCEL_ENV)
 if (import.meta.env.VITE_VERCEL_ENV==='production') {
-    apiUrl = import.meta.env.API_URL
+    apiUrl = import.meta.env.VITE_API_URL
 }
 
 export default apiUrl
