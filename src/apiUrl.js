c4c14631fff449d848167d909114ca781bf6600a
ignacioborraz <ignacioborraz@hotmail.com>
2023-06-26 19:49:18 -0300

    url

--- src/apiUrl.js
+++ src/apiUrl.js
@@ -1,10 +1,8 @@
 let apiUrl = 'http://localhost:8080/api'
 
-console.log(import.meta.env.NODE_ENV)
-if (import.meta.env.NODE_ENV==='production') {
+console.log(import.meta.env.VITE_VERCEL_ENV)
+if (import.meta.env.VITE_VERCEL_ENV==='production') {
     apiUrl = import.meta.env.API_URL
-    console.log(import.meta.env.API_URL)
-    console.log(apiUrl)
 }
 
 export default apiUrl
--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -9,7 +9,7 @@ export default function Carousel() {
     const [counter,setCounter] = useState(0)
     useEffect(
         ()=> {
-            console.log(apiUrl);
+            //console.log(apiUrl);
             axios(apiUrl+'/categories')
                 .then(res=> {
                     //console.log(res)
