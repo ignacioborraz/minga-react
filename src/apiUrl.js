c4f75c19ac1ec734b9e911abd408e6daa010ecf9
ignacioborraz <ignacioborraz@hotmail.com>
2023-06-26 19:46:34 -0300

    url

--- src/apiUrl.js
+++ src/apiUrl.js
@@ -1,8 +1,9 @@
 let apiUrl = 'http://localhost:8080/api'
 
-console.log(process.env.NODE_ENV)
-if (process.env.NODE_ENV==='production') {
+console.log(import.meta.env.NODE_ENV)
+if (import.meta.env.NODE_ENV==='production') {
     apiUrl = import.meta.env.API_URL
+    console.log(import.meta.env.API_URL)
     console.log(apiUrl)
 }
 
