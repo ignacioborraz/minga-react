ef9be398933aec7742124526fb7c2ba8907c0eae
ignacio.borraz <ignacio.borraz@koonolmexico.com>
2023-08-22 16:49:06 -0300

    m10

--- src/components/ButtonForm.jsx
+++ src/components/ButtonForm.jsx
@@ -1,11 +1,12 @@
 /* eslint-disable react/prop-types */
-export default function ButtonForm({ onClick, value }) {
+export default function ButtonForm({ onClick, value, color }) {
   return (
     <input
       onClick={onClick}
       className="w-[260px] md:w-[300px] lg:w-[360px] xl:w-[440px] h-[60px] p-2 my-[12px] text-xl text-white rounded-lg bg-[#F472B6] hover:drop-shadow-xl"
       type="button"
       value={value}
+      style={{backgroundColor:color}}
     />
   );
 }
