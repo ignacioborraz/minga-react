4297981e7582ecc01f4dfdff0bbb8304b6a580f7
ignacio.borraz <ignacioborraz@hotmail.com>
2023-12-15 19:18:24 -0300

    fix

--- src/apiUrl.js
+++ src/apiUrl.js
@@ -1,5 +1,5 @@
-let apiUrl = import.meta.env.PROD
+let apiUrl = /* import.meta.env.PROD
   ? import.meta.env.VITE_URL
-  : "http://localhost:8080/api";
+  :  */"http://localhost:8080/api";
 //console.log(apiUrl);
 export default apiUrl;
